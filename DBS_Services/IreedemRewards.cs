b591bf9b337533082a2eb528b9b2d85be8427e28
Raghuveer Subodha <Raghuveer Subodha>
2017-06-16 22:23:32 +0530

    Add project files.

---
+++ DBS_Services/IreedemRewards.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DBS_Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IreedemRewards" in both code and config file together.
+    [ServiceContract]
+    public interface IreedemRewards
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/DBS_Services/{custID}",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+
+        // String getRewards(String custID);
+        List<String> getRewards(String custID);
+    }
+}
---
+++ DBS_Services/reedemRewards.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DBS_Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "reedemRewards" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select reedemRewards.svc or reedemRewards.svc.cs at the Solution Explorer and start debugging.
+    public class reedemRewards : IreedemRewards
+    {
+
+        /*public string getRewards(string custID)
+        {
+            return "Service is up and running now";
+            throw new NotImplementedException();
+        }*/
+        List<String> rewardsArray = new List<String>();
+        public List<string> getRewards(String custID)
+        {
+            try
+            {
+               using (rewardsdbEntities dataCollector = new rewardsdbEntities())
+                {
+                    dataCollector.Database.Connection.Open();
+                    foreach (var dataRow in dataCollector.rewardsmasters)
+                    {
+                        rewardsArray.Add(dataRow.rewardDescription);
+                        rewardsArray.Add(dataRow.rewardName);
+                    }
+                }
+            } catch (Exception e)
+            {
+                throw new Exception("Exception occurred:", e);
+
+            }
+            return rewardsArray;
+        }
+    }
+}
+
